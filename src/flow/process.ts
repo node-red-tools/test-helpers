290fdf59f1c0ecb7e8e594c0668092963cf110d6
Tim Voronov <ziflex@gmail.com>
2020-01-31 17:02:26 -0500

    Added flow helper

---
+++ src/flow/process.ts
@@ -0,0 +1,79 @@
+import { spawn } from 'child_process';
+import { Writable } from 'stream';
+
+export type Probe = (data: string) => boolean;
+
+export type Termination = () => void;
+
+export interface Flow {
+    path?: string;
+    userDir?: string;
+    port?: number;
+    env?: { [key: string]: string };
+    settings?: string;
+    readinessProbe?: Probe;
+    stderr?: Writable;
+    stdout?: Writable;
+}
+
+export async function start(f: Flow = {}): Promise<Termination> {
+    return new Promise((resolve, reject) => {
+        const {
+            path = 'flows.json',
+            port = 1880,
+            userDir = '.',
+            env = {},
+            settings = 'settings.js',
+            stderr,
+            stdout,
+            readinessProbe,
+        } = f;
+        const args = [
+            'node-red',
+            '-p',
+            `${port}`,
+            '--userDir',
+            userDir,
+            '--settings',
+            settings,
+            path,
+        ];
+
+        const proc = spawn(`npx`, args, {
+            env: {
+                ...process.env,
+                ...env,
+            },
+        });
+
+        proc.on('error', reject);
+
+        if (stderr) {
+            proc.stderr.pipe(stderr);
+        }
+
+        if (stdout) {
+            proc.stdout.pipe(stdout);
+        }
+
+        const termination = () => proc.kill();
+        const probeWrapper = chunk => {
+            const str = String(chunk);
+            let resolved = false;
+
+            if (readinessProbe && readinessProbe(str)) {
+                resolve(termination);
+                resolved = true;
+            } else if (str.indexOf('Started flows') > -1) {
+                resolve(termination);
+                resolved = true;
+            }
+
+            if (resolved) {
+                proc.stdout.off('dta', probeWrapper);
+            }
+        };
+
+        proc.stdout.on('data', probeWrapper);
+    });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,42 @@
 import * as docker_ from './docker';
+import { Container, startAll, stopAll } from './docker/container';
+import { Flow, Termination, start } from './flow/process';
 
 export const docker = docker_;
+
+export interface Options {
+    containers?: Container[];
+    flow: Flow;
+}
+
+export interface Context {
+    containers?: string[];
+    flow: Termination;
+}
+
+export async function setup(opts: Options): Promise<Context> {
+    let containers: string[] | undefined = undefined;
+
+    if (opts.containers && opts.containers.length) {
+        containers = await startAll(opts.containers);
+    }
+
+    const flow = await start(opts.flow);
+
+    return {
+        containers,
+        flow,
+    };
+}
+
+export async function teardown(ctx: Context): Promise<void> {
+    if (!ctx) {
+        return Promise.reject(new Error('Missed context.'));
+    }
+
+    if (ctx.containers && ctx.containers.length) {
+        await stopAll(ctx.containers);
+    }
+
+    ctx.flow();
+}
