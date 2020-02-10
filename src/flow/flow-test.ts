e31c67cf4630730761d3a90d78e4deceee1a4f58
joe-gre <joseph88888888@gmail.com>
2020-02-10 11:05:26 -0500

    Flow Test (#7)
    * Create the flow test function, a generic function for testing flows by sending messages to the exchange and checking messages in output queus
    
    * Fixed flow start command to start the process correctly
    
    * Fix issues with starting and stopping amqp connections
    
    * Test for flow test and flow test fixes
    
    * Simplify createReceiveMessageCallback
    
    * Test flow test by creating queues in test and ensure consumers are cleaned up
    
    * Fixed flow test terminable order and not closing probe connection which were both causing "Unexpected close" exception; cleaned up test code
    
    * Allow flow test input or output to use HTTP or AMQP, additional flow test failure test, ensure test items are cleaned up, code comments
    
    * Update package.json
    
    * Changes based on PR feedback
    
    Co-authored-by: Tim Voronov <ziflex@users.noreply.github.com>

---
+++ src/flow/flow-test.ts
@@ -0,0 +1,167 @@
+import { Channel, Connection, ConsumeMessage } from "amqplib";
+import axios from 'axios';
+import { assert, expect } from 'chai';
+
+export enum IOType {
+    AMQP,
+    HTTP
+}
+
+export interface Input {
+    type: IOType;
+    payload: any;
+}
+
+export interface AmqpInput extends Input {
+    exchange: string;
+    routingKey: string;
+}
+
+export interface HttpInput extends Input {
+    url: string;
+}
+
+export interface Output {
+    type: IOType;
+    expectedOutput: any;
+}
+
+export interface AmqpOutput extends Output {
+    type: IOType;
+    queues: string[];
+    expectedQueue: string;
+}
+
+interface CreateMessageOut {
+    consumerPromise: Promise<void>;
+    messageCallback: (msg: ConsumeMessage | null) => any;
+}
+
+/**
+ * Returns a callback to be passed to channel.consume() and a promise that
+ * resolves or rejects when the callback processes a received message or the
+ * timeout is reached. Resolves when a message is received only when the message
+ * is expected to be received and the object equality assertion passes.
+ */
+function createReceiveMessageCallback(
+    expectedOutput: any,
+    outQueue: string,
+    shouldReceive: boolean
+): CreateMessageOut {
+    const res: any = {
+        resolve: undefined,
+        reject: undefined
+    };
+
+    const consumerPromise = new Promise<void>((resolve, reject) => {
+        res.resolve = resolve;
+        res.reject = reject;
+    });
+
+    let timeoutReached = false;
+    const timeout = setTimeout(() => {
+        timeoutReached = true;
+        if (shouldReceive) {
+            res.reject(new Error(`Queue ${outQueue} didn't receive a message before the timeout`));
+        } else {
+            res.resolve();
+        }
+    }, 6000);
+
+    return {
+        consumerPromise,
+        messageCallback: (msg: ConsumeMessage | null) => {
+            clearTimeout(timeout);
+
+            try {
+                if (msg === null) {
+                    throw new Error(`Output of queue ${outQueue} is null`);
+                }
+
+                if (!shouldReceive) {
+                    throw new Error(`Queue ${outQueue} received a message when it shouldn't have`);
+                }
+
+                const out = JSON.parse(msg.content.toString());
+
+                expect(out).to.deep.equal(expectedOutput, `Output of queue ${outQueue} does not match expected output`);
+                if (timeoutReached) {
+                    throw new Error(`Queue ${outQueue} received a message after the timeout was reached`);
+                }
+                res.resolve();
+            } catch (e) {
+                res.reject(e);
+            }
+        }
+    }
+}
+
+/**
+ * Test a flow by sending messages and making sure output matches what was expected.
+ * Flow input can be sent to an AMQP or HTTP server (likewise for expected flow output).
+ * If flow output is put on a queue it will make sure no other passed in queues received messages.
+ * A connection to an AMQP server must be passed in.
+ */
+export async function testFlow(
+    conn: Connection,
+    input: Input,
+    out: Output,
+): Promise<void> {
+    const consumers: Promise<void>[] = [];
+    let inputChan: Channel | undefined;
+    let outChannels: Channel[] = [];
+    const consumerTags: string[] = [];
+
+    try {
+        if (out.type === IOType.AMQP) {
+            outChannels = await Promise.all(
+            (out as AmqpOutput).queues.map(async (outQueue: string): Promise<Channel> => {
+                const outChan = await conn.createChannel();
+                // Clear any messages leftover from previous tests
+                await outChan.purgeQueue(outQueue);
+
+                const res = createReceiveMessageCallback(
+                    out.expectedOutput,
+                    outQueue,
+                    (out as AmqpOutput).expectedQueue === outQueue
+                );
+                const consumerTag = (await outChan.consume(outQueue, res.messageCallback)).consumerTag;
+                consumerTags.push(consumerTag);
+                consumers.push(res.consumerPromise);
+
+                return outChan;
+            }));
+        } else if (out.type === IOType.HTTP) {
+            // TODO
+            assert.fail("Http output tests not implemented");
+        } else {
+            assert.fail("Unknown flow test output type");
+        }
+
+        if (input.type === IOType.AMQP) {
+            inputChan = await conn.createChannel();
+            await inputChan.publish(
+                (input as AmqpInput).exchange,
+                (input as AmqpInput).routingKey,
+                Buffer.from(JSON.stringify(input.payload))
+            );
+        } else if (input.type === IOType.HTTP) {
+            await axios.get((input as HttpInput).url);
+        } else {
+            assert.fail("Unknown flow test input type");
+        }
+
+        await Promise.all(consumers);
+    } finally {
+        if (inputChan) {
+            await inputChan.close();
+        }
+
+        if (outChannels) {
+            await Promise.all(outChannels.map(async (outChan: Channel, idx: number) => {
+                await outChan.cancel(consumerTags[idx]);
+                await outChan.close();
+            }));
+        }
+    }
+}
